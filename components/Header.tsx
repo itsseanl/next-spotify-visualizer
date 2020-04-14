b8095da1f568db9f244c80a0c63501609b9ffe71
Sean Lyons <seanlyons@Seans-MacBook-Pro.local>
2020-04-14 18:04:30 -0400

    added Konva-React to modify canvas. Bars are set to modify circle but am experimenting with different track features to better sync movement

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,126 +1,132 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Visualizer from "./Visualizer";
+
 const Header = ({ authCode, url }) => {
-  const code = authCode;
+	const code = authCode;
 
-  const [currentSong, setCurrentSong] = useState(null);
+	const [currentSong, setCurrentSong] = useState(null);
 
-  const [songTitle, setSongTitle] = useState("initial value");
-  const [albumTitle, setAlbumTitle] = useState("");
-  const [albumArt, setAlbumArt] = useState("");
+	const [songTitle, setSongTitle] = useState("initial value");
+	const [albumTitle, setAlbumTitle] = useState("");
+	const [albumArt, setAlbumArt] = useState("");
 
-  const [artistTitle, setArtistTitle] = useState("");
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const getCurrentSong = async code => {
-        const getSong = await fetch(
-          "https://api.spotify.com/v1/me/player/currently-playing",
-          {
-            headers: {
-              Accept: "application/json",
-              Authorization: "Bearer " + code,
-              "Content-Type": "application/json"
-            }
-          }
-        );
+	const [artistTitle, setArtistTitle] = useState("");
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const getCurrentSong = async (code) => {
+				const getSong = await fetch(
+					"https://api.spotify.com/v1/me/player/currently-playing",
+					{
+						headers: {
+							Accept: "application/json",
+							Authorization: "Bearer " + code,
+							"Content-Type": "application/json",
+						},
+					}
+				);
 
-        const getCurrentSong = await getSong.json();
-        try {
-          if (getCurrentSong.item.name === songTitle) {
-            return;
-          } else {
-            try {
-              console.log("try");
-              setCurrentSong(getCurrentSong);
+				const getCurrentSong = await getSong.json();
+				try {
+					if (getCurrentSong.item.name === songTitle) {
+						return;
+					} else {
+						try {
+							console.log("try");
+							setCurrentSong(getCurrentSong);
 
-              setAlbumArt(getCurrentSong.item.album.images[0].url);
-              setSongTitle(getCurrentSong.item.name);
-              setAlbumTitle(getCurrentSong.item.album.name);
-              setArtistTitle(getCurrentSong.item.album.artists[0].name);
-            } catch {
-              window.location.href = "/";
-            }
-          }
-        } catch {
-          console.log("err");
-        }
-      };
+							setAlbumArt(getCurrentSong.item.album.images[0].url);
+							setSongTitle(getCurrentSong.item.name);
+							setAlbumTitle(getCurrentSong.item.album.name);
+							setArtistTitle(getCurrentSong.item.album.artists[0].name);
+						} catch {
+							window.location.href = "/";
+						}
+					}
+				} catch {
+					console.log("err");
+				}
+			};
 
-      getCurrentSong(code);
-    }, 1000);
+			getCurrentSong(code);
+		}, 1000);
 
-    return () => clearInterval(interval);
-  }, [code, songTitle, albumArt]);
+		return () => clearInterval(interval);
+	}, [code, songTitle, albumArt]);
 
-  return (
-    <>
-      <header>
-        <div className="left"></div>
-        <div className="right">
-          <div className="info">
-            <p>{songTitle}</p>
-            <p>{albumTitle}</p>
-            <p>{artistTitle}</p>
-          </div>
-          <img src={albumArt} />
-        </div>
-      </header>
-      {currentSong != null ? (
-        <Visualizer authCode={code} theCurrentSong={currentSong} />
-      ) : // <p>visualizer</p>
-      null}
-      <style jsx>{`
-        header {
-          display: flex;
-          flex-wrap: nowrap;
-          justify-content: space-between;
-          height: 20vh;
-          background: rgba(0, 0, 0, 0.6);
-          width: 100%;
-          z-index: 99;
-          position: relative;
-        }
+	return (
+		<>
+			<header>
+				<div className="left"></div>
+				<div className="right">
+					<div className="info">
+						<p>{songTitle}</p>
+						<p>{albumTitle}</p>
+						<p>{artistTitle}</p>
+					</div>
+					<img src={albumArt} />
+				</div>
+			</header>
+			{currentSong != null ? (
+				<div className="vis-wrapper">
+					<Visualizer authCode={code} theCurrentSong={currentSong} />
+				</div>
+			) : // <p>visualizer</p>
+			null}
+			<style jsx>{`
+				header {
+					display: flex;
+					flex-wrap: nowrap;
+					justify-content: space-between;
+					height: 20vh;
+					background: rgba(0, 0, 0, 0.6);
+					width: 100%;
+					z-index: 99;
+					position: relative;
+				}
 
-        .left > h1 {
-          font-size: 18px;
-        }
-        .left > h3 {
-          font-size: 14px;
-        }
-        .right {
-          display: flex;
-        }
-        .info {
-          display: flex;
-          flex-direction: column;
-          justify-content: flex-end;
-          padding-right: 5px;
-        }
+				.left > h1 {
+					font-size: 18px;
+				}
+				.left > h3 {
+					font-size: 14px;
+				}
+				.right {
+					display: flex;
+				}
+				.info {
+					display: flex;
+					flex-direction: column;
+					justify-content: flex-end;
+					padding-right: 5px;
+				}
 
-        .right img {
-          width: auto;
-          height: 100%;
-        }
-        .right p {
-          margin: 0;
-          padding: 2px;
-          font-size: 12px;
-          color: #fffff0;
-          font-family: "Open sans", sans-serif;
-        }
-        .right p:nth-child(1) {
-          font-weight: 400;
-        }
-        .right p:nth-child(2) {
-          font-weight: 700;
-        }
-        .right p:nth-child(3) {
-          font-weight: 400;
-        }
-      `}</style>
-    </>
-  );
+				.right img {
+					width: auto;
+					height: 100%;
+				}
+				.right p {
+					margin: 0;
+					padding: 2px;
+					font-size: 12px;
+					color: #fffff0;
+					font-family: "Open sans", sans-serif;
+				}
+				.right p:nth-child(1) {
+					font-weight: 400;
+				}
+				.right p:nth-child(2) {
+					font-weight: 700;
+				}
+				.right p:nth-child(3) {
+					font-weight: 400;
+				}
+				.vis-wrapper {
+					margin-top: -20vh;
+				}
+			`}</style>
+		</>
+	);
 };
 
 export default Header;
--- components/Visualizer.tsx
+++ components/Visualizer.tsx
@@ -2,143 +2,172 @@ import React, { useRef, useState, useEffect } from "react";
 import Link from "next/link";
 import { array } from "prop-types";
 import { getEnabledCategories } from "trace_events";
+import { Stage, Layer, Rect, Text, Circle } from "react-konva";
+import Konva from "konva";
+
 const Visualizer = ({ authCode, theCurrentSong }) => {
-  //   const [theCurrentSong, settheCurrentSong] = useState(thetheCurrentSong);
+	//   const [theCurrentSong, settheCurrentSong] = useState(thetheCurrentSong);
 
-  const [audioAnalysis, setAudioAnalysis] = useState(null);
-  const [trackFeatures, setTrackFeatures] = useState(null);
-  const [randomColor, setRandomColor] = useState(null);
-  const [bpm, setBPM] = useState(null);
-  const [currentTime, setCurrentTime] = useState(0);
-  const [songBars, setSongBars] = useState(null);
+	const [audioAnalysis, setAudioAnalysis] = useState(null);
+	const [trackFeatures, setTrackFeatures] = useState(null);
+	const [randomColor, setRandomColor] = useState(null);
+	const [bpm, setBPM] = useState(null);
+	const [currentTime, setCurrentTime] = useState(0);
+	const [songBars, setSongBars] = useState(null);
 
-  const [boolin, setBoolin] = useState(false);
+	const [boolin, setBoolin] = useState(false);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      try {
-        songBars.forEach(start => {
-          //   console.log(Math.trunc(start));
-          //   console.log(Math.trunc(currentTime / 1000));
-          //   console.log(start);
-          if (Math.trunc(start) == Math.trunc(currentTime / 1000)) {
-            console.log(
-              "start: " +
-                Math.trunc(start) +
-                " current: " +
-                Math.trunc(currentTime / 1000) +
-                " hit"
-            );
-          }
-        });
-      } catch {
-        console.log("no songbars");
-      }
-      setCurrentTime(currentTime + 1000);
-    }, 1000);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			try {
+				songBars.forEach((start) => {
+					//   console.log(Math.trunc(start));
+					//   console.log(Math.trunc(currentTime / 1000));
+					//   console.log(start);
+					if (Math.trunc(start) == Math.trunc(currentTime / 1000)) {
+						console.log(
+							"start: " +
+								Math.trunc(start) +
+								" current: " +
+								Math.trunc(currentTime / 1000) +
+								" hit"
+						);
+						changeSize();
+					}
+				});
+			} catch {
+				console.log("no songbars");
+			}
+			setCurrentTime(currentTime + 100);
+		}, 100);
 
-    return () => clearInterval(interval);
-  }, [songBars, currentTime]);
+		return () => clearInterval(interval);
+	}, [songBars, currentTime]);
 
-  useEffect(() => {
-    if (audioAnalysis != null) {
-      //audioAnalysis provides an array of every bar, beat, etc.
-      //   console.log(audioAnalysis.bars);
-      const songBars = [];
-      audioAnalysis.bars.map(theBars => {
-        // console.log(currentTime / 1000);
-        // console.log(theBars.start);
-        songBars.push(theBars.start);
-      });
-      console.log(songBars);
+	useEffect(() => {
+		if (audioAnalysis != null) {
+			//audioAnalysis provides an array of every bar, beat, etc.
+			//   console.log(audioAnalysis.bars);
+			const songBars = [];
+			audioAnalysis.bars.map((theBars) => {
+				// console.log(currentTime / 1000);
+				// console.log(theBars.start);
+				songBars.push(theBars.start);
+			});
+			console.log(songBars);
 
-      setSongBars(songBars);
-      //   console.log(Math.trunc(trackFeatures.duration_ms / 1000));
-      //   console.log(Math.trunc(currentTime / 1000));
-    }
-  }, [audioAnalysis]);
+			setSongBars(songBars);
+			//   console.log(Math.trunc(trackFeatures.duration_ms / 1000));
+			//   console.log(Math.trunc(currentTime / 1000));
+		}
+	}, [audioAnalysis]);
 
-  useEffect(() => {
-    setBoolin(false);
-    setCurrentTime(theCurrentSong.progress_ms + 2000);
-    getAudioFeatures(theCurrentSong, authCode);
-  }, [theCurrentSong]);
+	useEffect(() => {
+		setBoolin(false);
+		setCurrentTime(theCurrentSong.progress_ms + 2000);
+		getAudioFeatures(theCurrentSong, authCode);
+	}, [theCurrentSong]);
 
-  //set background color to change with tempo
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (bpm != null) {
-        // console.log(bpm);
-        // console.log(bpm);
+	//set background color to change with tempo
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (bpm != null) {
+				// console.log(bpm);
+				// console.log(bpm);
 
-        setRandomColor(
-          "rgba(" +
-            Math.floor(Math.random() * 256) +
-            "," +
-            Math.floor(Math.random() * 256) +
-            "," +
-            Math.floor(Math.random() * 256) +
-            ",0.8)"
-        );
-      }
-    }, 60000 / bpm);
-    return () => clearInterval(interval);
-  }, [bpm]);
+				setRandomColor(
+					"rgba(" +
+						Math.floor(Math.random() * 256) +
+						"," +
+						Math.floor(Math.random() * 256) +
+						"," +
+						Math.floor(Math.random() * 256) +
+						",0.8)"
+				);
+			}
+		}, 60000 / bpm);
+		return () => clearInterval(interval);
+	}, [bpm]);
 
-  async function getAudioFeatures(theCurrentSong, authCode) {
-    setBoolin(true);
-    console.log("audioFeaturesRunning");
-    let trackFeatures = new Object();
-    const audioFeaturesEndpt =
-      "https://api.spotify.com/v1/audio-features/" + theCurrentSong.item.id;
-    const audioFeatures = await fetch(audioFeaturesEndpt, {
-      headers: {
-        Accept: "application/json",
-        Authorization: "Bearer " + authCode,
-        "Content-Type": "application/json"
-      }
-    });
-    const theTrackFeatures = await audioFeatures.json();
-    setTrackFeatures(theTrackFeatures);
+	async function getAudioFeatures(theCurrentSong, authCode) {
+		setBoolin(true);
+		console.log("audioFeaturesRunning");
+		let trackFeatures = new Object();
+		const audioFeaturesEndpt =
+			"https://api.spotify.com/v1/audio-features/" + theCurrentSong.item.id;
+		const audioFeatures = await fetch(audioFeaturesEndpt, {
+			headers: {
+				Accept: "application/json",
+				Authorization: "Bearer " + authCode,
+				"Content-Type": "application/json",
+			},
+		});
+		const theTrackFeatures = await audioFeatures.json();
+		setTrackFeatures(theTrackFeatures);
 
-    console.log("theCurrentSong item id: " + theCurrentSong.item.id);
+		console.log("theCurrentSong item id: " + theCurrentSong.item.id);
 
-    const getAnalysis = await fetch(
-      "https://api.spotify.com/v1/audio-analysis/" + theCurrentSong.item.id,
-      {
-        headers: {
-          Accept: "application/json",
-          Authorization: "Bearer " + authCode,
-          "Content-Type": "application/json"
-        }
-      }
-    );
-    const theAnalysis = await getAnalysis.json();
-    setAudioAnalysis(theAnalysis);
+		const getAnalysis = await fetch(
+			"https://api.spotify.com/v1/audio-analysis/" + theCurrentSong.item.id,
+			{
+				headers: {
+					Accept: "application/json",
+					Authorization: "Bearer " + authCode,
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const theAnalysis = await getAnalysis.json();
+		setAudioAnalysis(theAnalysis);
 
-    if (theTrackFeatures.tempo == bpm) {
-      console.log("nope");
-    } else {
-      setBPM(theTrackFeatures.tempo);
-    }
-  }
-  //   console.log(theCurrentSong);
+		if (theTrackFeatures.tempo == bpm) {
+			console.log("nope");
+		} else {
+			setBPM(theTrackFeatures.tempo);
+		}
+	}
+	const theCirc = useRef(null);
+	const changeSize = () => {
+		// to() is a method of `Konva.Node` instances
+		theCirc.current.to({
+			scaleX: Math.random() + 0.8,
+			scaleY: Math.random() + 0.8,
+			duration: 0.1,
+		});
+	};
 
-  return (
-    <>
-      <canvas className="theCanvas" />
-
-      <style jsx>{`
-        .theCanvas {
-          background-color: ${randomColor};
-          transition: 0.15s all;
-          width: 100%;
-          height: 100vh;
-          margin-top: -20vh;
-        }
-      `}</style>
-    </>
-  );
+	console.log(audioAnalysis);
+	// console.log(bpm);
+	return (
+		<>
+			{/* <canvas className="theCanvas" /> */}
+			<Stage
+				className="stage"
+				width={window.innerWidth}
+				height={window.innerHeight}
+				fill={randomColor}
+			>
+				<Layer fill={randomColor}>
+					<Rect
+						x={0}
+						y={0}
+						width={window.innerWidth}
+						height={window.innerHeight}
+						fill={randomColor}
+						shadowBlur={5}
+					/>
+					<Circle
+						ref={theCirc}
+						x={window.innerWidth / 2}
+						y={window.innerHeight / 2}
+						fill={"rgba(255,255,0,0.4)"}
+						radius={70}
+					/>
+				</Layer>
+			</Stage>
+			<style jsx>{``}</style>
+		</>
+	);
 };
 
 export default Visualizer;
